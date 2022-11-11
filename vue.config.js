d66592b4629558fe85cb5aa976b011e4a6a96e62
tonight11 <godofspeedman@gmail.com>
2022-11-11 18:53:06 +0300

    123

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    __VUE_I18N_FULL_INSTALL__: JSON.stringify(true),
-    __INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
-    __VUE_I18N_LEGACY_API__: JSON.stringify(false),
-
-    chainWebpack: (config) => {
-        config.resolve.alias.set('vue-i18n', 'vue-i18n/dist/vue-i18n.cjs.js')
+    _pluginOptions: {
+        i18n: {
+            locale: 'ja',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false,
+            enableLegacy: false,
+        },
     },
 })
